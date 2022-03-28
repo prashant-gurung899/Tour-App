d89b406889dfc25c99337d4aa0e637825cd0da69
Prashant Gurung <prasantgrg777@gmail.com>
2022-03-28 12:47:40 -0700

    DOCUMENT MIDDLEWARE ADDED

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 //Schema
 const tourSchema = new mongoose.Schema(
   {
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -66,6 +68,23 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+//DOCUMENT MIDDLEWARE : runs before .save() and .create() ONLY
+tourSchema.pre('save', function (next) {
+  // console.log(this); //this points to the curently processed document
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function (next) {
+//   console.log('WILL SAVE DOCUMENT...');
+//   next();
+// });
+
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 //create model from Schema
 const Tour = mongoose.model('Tour', tourSchema);
